f71a2e54fb74ece6799db45ffcb6b11c58657a06
Caosonnw <maivucaoson2004@gmail.com>
2024-01-23 09:59:53 +0700

    trang login va trang register, thanh bottom menu

--- app/src/main/java/com/example/car4rent/MainActivity.java
+++ app/src/main/java/com/example/car4rent/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.car4rent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Notification;
 import android.os.Bundle;
+
+import com.example.car4rent.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            final int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.notification) {
+                replaceFragment(new RegisterFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new LoginFragment());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
+
