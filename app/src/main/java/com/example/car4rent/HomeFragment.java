650a11590d73f6d7b2eb2cdfccfa599dbab823c8
Caosonnw <maivucaoson2004@gmail.com>
2024-02-24 11:28:43 +0700

    new updates

--- app/src/main/java/com/example/car4rent/HomeFragment.java
+++ app/src/main/java/com/example/car4rent/HomeFragment.java
@@ -1,7 +1,6 @@
 package com.example.car4rent;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -19,8 +18,6 @@ import android.widget.Button;
 public class HomeFragment extends Fragment {
     private Button btnXeTuLai;
     private Button btnXeCoTaiXe;
-
-
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
