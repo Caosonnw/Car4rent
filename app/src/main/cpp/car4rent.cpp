f578034a549da4769a127447ba9caa7cee27d8f1
Hoang Vi <nguyenhoangvy5761@gmail.com>
2024-03-08 12:55:40 +0700

    update trang profilr

---
+++ app/src/main/cpp/car4rent.cpp
@@ -0,0 +1,17 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("car4rent");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("car4rent")
+//      }
+//    }
